5631902933a978b5d8611d38576b481efa89ffb2
Stanislaw-Golebiewski <slowik66@o2.pl>
2019-05-21 12:50:15 +0200

    add new modules

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ with open(IN_FILE_NAME, 'r') as f:
 cv2.namedWindow("image")
 cv2.moveWindow("image", 0, 0)
 
+# frame counter
 f_counter = 0
 start = time.time()
 
 game = FlappyBird()
+current_fps = 0
 
 with mss.mss() as sct:
     while True:
@@ -27,14 +29,16 @@ with mss.mss() as sct:
         screen_img = np.array(sct.grab(cut_area))
         fb_data = game.image_to_data(screen_img)
         fb_image = game.data_to_image(fb_data, screen_img)
+        cv2.putText(fb_image, str(int(current_fps)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         cv2.imshow("image", fb_image)
         k = cv2.waitKey(1)
         if k == 27:
             break
+
         # fps
         if f_counter == 60:
             stop = time.time()
-            print(f"fps: {f_counter / (stop - start)}")
+            current_fps = f_counter / (stop - start)
             start = time.time()
             f_counter = 0
 
